a28905524b8c0d457dd2de509f76d5314cef79a9
KovalVladuslav <koval.vladuslav.v@gmail.com>
2025-02-24 21:15:13 +0200

    hm -7

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,21 @@
-class Storage {
-  #items;
-  constructor(items) {
-    this.#items = items;
-  }
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
 
-  getItems() {
-    return this.#items;
-  }
+const gallery = document.querySelector(".gallery");
 
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    const updatedItems = this.#items.filter((item) => item !== itemToRemove);
-
-    this.#items = updatedItems;
-  }
-}
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+const markup = images
+  .map(({ url, alt }) => `<li class="gallery-item"><img src="${url}" alt="${alt}" class="gallery-img"></li>`)
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
